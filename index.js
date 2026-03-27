05aea16fa067c52fce25a9f830e5e48b28c7f129
La Fronterisima <159441596+lafronterisima@users.noreply.github.com>
2026-03-26 21:02:55 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -116,6 +116,6 @@ const PORT = process.env.PORT || 10000;
 http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Radio IA funcionando 🎧");
-}).listen(PORT, () => {
-    console.log("🌐 Servidor corriendo en puerto", PORT);
+}).listen(PORT, "0.0.0.0", () => {
+    console.log("🌐 Servidor activo en puerto", PORT);
 });
