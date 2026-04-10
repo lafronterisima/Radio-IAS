e49c47eaf6be80587a58f21ac5ce7bf1e1695afc
La Fronterisima <159441596+lafronterisima@users.noreply.github.com>
2026-04-10 18:09:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ const KEYS = {
     STATION_ID: sID,
     PASSWORD: safeTrim(process.env.APP_PASSWORD),
     TELEGRAM_TOKEN: safeTrim(process.env.TELEGRAM_TOKEN),
-    YOUTUBE: safeTrim(process.env.YOUTUBE_API_KEY),
-  
+    YOUTUBE: safeTrim(process.env.YOUTUBE_KEY),
 };
 
 // ======= 2. INICIALIZACIÓN DE CLIENTES =======
