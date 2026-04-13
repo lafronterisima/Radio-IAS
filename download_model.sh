442d77025c8e08ab3ec3b020cbd47e95f25cb10d
La Fronterisima <159441596+lafronterisima@users.noreply.github.com>
2026-04-13 12:31:05 -0500

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-mkdir -p modelos
-cd modelos
-
-# URL del modelo Salomé SMALL (Mucho más liviano)
-MODEL_URL="https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-piper-es_CO-salome-low.tar.bz2"
-FILE="vits-piper-es_CO-salome-low.tar.bz2"
-
-if [ ! -f "vits-piper-es_CO-salome-low/es_CO-salome-low.onnx" ]; then
-    echo "⬇️ Descargando modelo LIVIANO (Salomé)..."
-    curl -L -o "$FILE" "$MODEL_URL"
-
-    if [ -f "$FILE" ] && [ $(wc -c <"$FILE") -ge 5000000 ]; then
-        echo "📦 Descomprimiendo..."
-        tar xjf "$FILE"
-        rm "$FILE"
-        echo "✅ Modelo liviano listo."
-    else
-        echo "❌ Error en descarga."
-        exit 1
-    fi
-else
-    echo "✔ El modelo ya existe."
+mkdir -p modelos/vits-piper-es_CO-salome-low
+cd modelos/vits-piper-es_CO-salome-low
+if [ ! -f "es_CO-salome-low.onnx" ]; then
+  wget https://huggingface.co/csukuangfj/vits-piper-es_CO-salome-low/resolve/main/es_CO-salome-low.onnx
+  wget https://huggingface.co/csukuangfj/vits-piper-es_CO-salome-low/resolve/main/tokens.txt
+  # Descargar carpeta espeak-ng-data si es necesario
 fi
